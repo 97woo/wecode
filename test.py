841e3a6edf4240b9855bc362a65028dd555521cb
park geon woo <parkgeonwoo@parks-MacBook-Air.local>
2022-02-25 11:13:19 +0900

    calu

---
+++ test.py
@@ -0,0 +1,21 @@
+
+from ast import Pass
+from urllib.parse import ParseResultBytes
+
+
+def calu():
+    sum = 0
+    result = []
+    for i in range(1,51):
+        if i % 2 == 0:
+            
+            result.append(i)
+        
+        
+            
+            
+    return result
+   
+print(calu())
+        
+        
